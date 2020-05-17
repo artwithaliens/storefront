bda62e87d61fe72effbb2e2dc4e6300852842c92
Eike Foken <eikefoken@gmail.com>
2020-05-17 14:54:21 +0200

    Change app icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,9 @@ export default class extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Amatic+SC:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
