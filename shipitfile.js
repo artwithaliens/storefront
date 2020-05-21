27572a45bbe5b516b571250b7575a61fe0077049
Eike Foken <eikefoken@gmail.com>
2020-05-21 15:47:48 +0200

    Update shipitfile.js

--- shipitfile.js
+++ shipitfile.js
@@ -1,19 +1,34 @@
 /* eslint-env node */
 /* eslint-disable import/no-extraneous-dependencies */
 const deploy = require('shipit-deploy');
+const shared = require('shipit-shared');
 
 module.exports = (shipit) => {
   // Load shipit-deploy tasks
   deploy(shipit);
+  shared(shipit);
+
+  shipit.task('build', () => {
+    shipit.remote(`cd ${shipit.releasePath} && yarn install --frozen-lockfile && yarn build`);
+    shipit.emit('built');
+  });
+
+  shipit.on('updated', () => {
+    shipit.start('build');
+  });
 
   shipit.initConfig({
     default: {
       branch: 'master',
       deployTo: '/var/www/storefront',
-      repositoryUrl: 'https://github.com/efoken/artwithaliens-storefront.git',
+      repositoryUrl: 'https://github.com/artwithaliens/storefront.git',
+      shared: {
+        dirs: [],
+        files: ['.env'],
+      },
     },
     production: {
-      servers: 'deploy@artwithaliens.com',
+      servers: 'deploy@45.9.61.163',
     },
   });
 };
