fc6fbe954d3b5ac3bd78600e3066f783338f980f
Eike Foken <eikefoken@gmail.com>
2020-06-21 15:53:00 +0200

    Get rid of Sentry webpack plugin for now

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 const withOffline = require('next-offline');
-const merge = require('lodash/merge');
 const { version } = require('./package.json');
 
 module.exports = withOffline({
+  generateSw: false,
   workboxOpts: {
+    swSrc: './src/serviceWorker.ts',
     swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
   },
   env: {
     SITE_URL: process.env.SITE_URL,
@@ -29,23 +17,6 @@ module.exports = withOffline({
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
     GA_TRACKING_ID: 'UA-127038775-1',
   },
-  webpack: (config) =>
-    merge(
-      config,
-      process.env.SENTRY_DSN &&
-        process.env.SENTRY_ORG &&
-        process.env.SENTRY_PROJECT &&
-        process.env.SENTRY_AUTH_TOKEN &&
-        process.env.NODE_ENV === 'production' && {
-          plugins: [
-            new SentryWebpackPlugin({
-              include: '.next',
-              ignore: ['node_modules'],
-              urlPrefix: '~/_next',
-            }),
-          ],
-        },
-    ),
   experimental: {
     plugins: true,
     rewrites: async () => [
