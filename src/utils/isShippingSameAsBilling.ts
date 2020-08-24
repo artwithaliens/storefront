db8cfaa234bbd776750ee2c8c58aa78e0d60f682
Eike Foken <eikefoken@gmail.com>
2020-08-24 19:18:47 +0200

    Fix shipping equals billing if empty

--- src/utils/isShippingSameAsBilling.ts
+++ src/utils/isShippingSameAsBilling.ts
@@ -6,8 +6,20 @@ export default function isShippingSameAsBilling(
   shipping?: CustomerAddressInput | null,
   billing?: CustomerAddressInput | null,
 ) {
-  return isEqual(
-    omit(shipping, ['email', 'overwrite', 'phone']),
-    omit(billing, ['email', 'overwrite', 'phone']),
+  return (
+    shipping?.firstName != null &&
+    shipping?.lastName != null &&
+    shipping?.address1 != null &&
+    shipping?.city != null &&
+    shipping?.postcode != null &&
+    billing?.firstName != null &&
+    billing?.lastName != null &&
+    billing?.address1 != null &&
+    billing?.city != null &&
+    billing?.postcode != null &&
+    isEqual(
+      omit(shipping, ['email', 'overwrite', 'phone']),
+      omit(billing, ['email', 'overwrite', 'phone']),
+    )
   );
 }
