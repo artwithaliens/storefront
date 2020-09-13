7153d5f07b0ba80bdf496d1891c52324a39c5670
Eike Foken <eikefoken@gmail.com>
2020-09-13 14:40:36 +0200

    Do some improvements of product detail page

--- src/components/ProductGallery/ProductGallery.tsx
+++ src/components/ProductGallery/ProductGallery.tsx
@@ -1,15 +1,26 @@
-import { makeStyles } from '@material-ui/core';
-import React from 'react';
+import { Hidden, makeStyles, MobileStepper } from '@material-ui/core';
+import React, { useState } from 'react';
+import SwipeableViews from 'react-swipeable-views';
 import { ProductQuery } from '../../graphql';
 
 const useStyles = makeStyles(
   ({ spacing }) => ({
-    root: {
+    stepper: {
+      flexGrow: 1,
+    },
+
+    stepperImage: {
+      display: 'block',
+      overflow: 'hidden',
+      width: '100%',
+    },
+
+    gallery: {
       display: 'flex',
       flexDirection: 'column',
     },
 
-    image: {
+    galleryImage: {
       height: 'auto',
       marginBottom: spacing(1),
       maxWidth: '100%',
@@ -24,30 +35,77 @@ type Props = {
 
 const ProductGallery: React.FC<Props> = ({ product }) => {
   const styles = useStyles();
+  const [activeStep, setActiveStep] = useState(0);
 
   return (
-    <div className={styles.root}>
-      <img
-        className={styles.image}
-        src={product.image?.sourceUrl ?? undefined}
-        srcSet={product.image?.srcSet ?? undefined}
-        sizes={product.image?.sizes ?? undefined}
-        alt={product.image?.altText ?? ''}
-      />
-      {product.galleryImages?.nodes?.map(
-        (mediaItem) =>
-          mediaItem != null && (
-            <img
-              key={mediaItem.id}
-              className={styles.image}
-              src={mediaItem.sourceUrl ?? undefined}
-              srcSet={mediaItem.srcSet ?? undefined}
-              sizes={mediaItem.sizes ?? undefined}
-              alt={mediaItem.altText ?? ''}
-            />
-          ),
-      )}
-    </div>
+    <>
+      <Hidden mdUp>
+        <div className={styles.stepper}>
+          <SwipeableViews
+            enableMouseEvents
+            axis="x"
+            index={activeStep}
+            onChangeIndex={(index) => setActiveStep(index)}
+          >
+            <div>
+              <img
+                className={styles.stepperImage}
+                src={product.image?.sourceUrl ?? undefined}
+                srcSet={product.image?.srcSet ?? undefined}
+                sizes={product.image?.sizes ?? undefined}
+                alt={product.image?.altText ?? ''}
+              />
+            </div>
+            {product.galleryImages?.nodes?.map(
+              (mediaItem) =>
+                mediaItem != null && (
+                  <div key={mediaItem.id}>
+                    <img
+                      className={styles.stepperImage}
+                      src={mediaItem.sourceUrl ?? undefined}
+                      srcSet={mediaItem.srcSet ?? undefined}
+                      sizes={mediaItem.sizes ?? undefined}
+                      alt={mediaItem.altText ?? ''}
+                    />
+                  </div>
+                ),
+            )}
+          </SwipeableViews>
+          <MobileStepper
+            steps={(product.galleryImages?.nodes?.length ?? 0) + 1}
+            variant="dots"
+            position="static"
+            activeStep={activeStep}
+            nextButton={<div />}
+            backButton={<div />}
+          />
+        </div>
+      </Hidden>
+      <Hidden smDown>
+        <div className={styles.gallery}>
+          <img
+            className={styles.galleryImage}
+            src={product.image?.sourceUrl ?? undefined}
+            srcSet={product.image?.srcSet ?? undefined}
+            sizes={product.image?.sizes ?? undefined}
+            alt={product.image?.altText ?? ''}
+          />
+          {product.galleryImages?.nodes?.map(
+            (mediaItem) =>
+              mediaItem != null && (
+                <img
+                  key={mediaItem.id}
+                  className={styles.galleryImage}
+                  src={mediaItem.sourceUrl ?? undefined}
+                  srcSet={mediaItem.srcSet ?? undefined}
+                  sizes={mediaItem.sizes ?? undefined}
+                  alt={mediaItem.altText ?? ''}
+                />
+              ),
+          )}
+        </div>
+      </Hidden>
+    </>
   );
 };
 
--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,44 +1,21 @@
-import { Box, Container, Grid, Hidden, makeStyles, Typography } from '@material-ui/core';
-import { first } from 'lodash';
+import { Box, Container, Grid, Typography } from '@material-ui/core';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import Layout from '../../components/Layout';
-import Link from '../../components/Link';
-import Price from '../../components/Price';
 import ProductAddToCart from '../../components/ProductAddToCart';
 import ProductAttributes from '../../components/ProductAttributes';
 import ProductGallery from '../../components/ProductGallery';
-import ProductGalleryStepper from '../../components/ProductGalleryStepper';
 import ProductGrid from '../../components/ProductGrid';
 import ProductMeta from '../../components/ProductMeta';
+import ProductSummary from '../../components/ProductSummary';
 import RichText from '../../components/RichText';
-import { StockStatusEnum, useProductQuery } from '../../graphql';
+import { useProductQuery } from '../../graphql';
 import isBlank from '../../utils/isBlank';
 
-const useStyles = makeStyles(({ breakpoints, spacing }) => ({
-  container: {
-    paddingBottom: spacing(1),
-
-    [breakpoints.down('sm')]: {
-      overflow: 'hidden',
-    },
-  },
-
-  description: {
-    position: 'sticky',
-    top: 48,
-  },
-
-  meta: {
-    marginTop: 16,
-  },
-}));
-
 const Product: NextPage = () => {
-  const styles = useStyles();
   const router = useRouter();
 
   const { data: { product } = { product: undefined } } = useProductQuery({
@@ -52,7 +29,7 @@ const Product: NextPage = () => {
       ReactGA.plugin.execute('ec', 'addProduct', {
         id: product.id,
         name: product.name,
-        category: first(product.productCategories?.nodes)?.name,
+        category: product.productCategories?.nodes?.[0]?.name,
         price:
           (product.__typename === 'SimpleProduct' ||
             product.__typename === 'VariableProduct' ||
@@ -88,54 +65,18 @@ const Product: NextPage = () => {
         }}
       />
       {product != null && (
-        <Container className={styles.container}>
+        <Container>
           <Box my={6}>
             <Grid container spacing={6}>
               <Grid item md={6}>
-                <Hidden mdUp>
-                  <ProductGalleryStepper product={product} />
-                </Hidden>
-                <Hidden smDown>
-                  <ProductGallery product={product} />
-                </Hidden>
+                <ProductGallery product={product} />
               </Grid>
               <Grid item md={6}>
-                <div className={styles.description}>
-                  <Typography component="h1" variant="h2">
-                    {product.name}
-                  </Typography>
-                  {(product.__typename === 'SimpleProduct' ||
-                    product.__typename === 'VariableProduct' ||
-                    product.__typename === 'ExternalProduct') && (
-                    <Price color="primary" variant="h3">
-                      {product.price}
-                    </Price>
-                  )}
-                  <Typography gutterBottom variant="body2">
-                    plus{' '}
-                    <Link href="/[slug]" as="/shipping-costs">
-                      Shipping Costs
-                    </Link>
-                  </Typography>
-                  <RichText>
-                    {isBlank(product.shortDescription)
-                      ? product.description
-                      : product.shortDescription}
-                  </RichText>
-                  {product.__typename === 'SimpleProduct' &&
-                  product.stockStatus === StockStatusEnum.OUT_OF_STOCK ? (
-                    <Typography gutterBottom color="error" variant="h5">
-                      Sold out
-                    </Typography>
-                  ) : (
-                    <Typography gutterBottom variant="body2">
-                      Deliverable in{' '}
-                      {isBlank(product.deliveryTime) ? '2-3 working days' : product.deliveryTime}
-                    </Typography>
-                  )}
+                <Box position="sticky" top={48}>
+                  <ProductSummary product={product} />
                   <ProductAddToCart product={product} />
                   <ProductMeta product={product} />
-                </div>
+                </Box>
               </Grid>
             </Grid>
             {!isBlank(product.shortDescription) && !isBlank(product.description) && (
