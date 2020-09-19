69df6d57da6c76ee0634951dd6a384fd7bac2233
Eike Foken <eikefoken@gmail.com>
2020-09-19 20:19:56 +0200

    Change custom App with hooks as Next.js recommends

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,12 +9,12 @@ import {
 import { getDataFromTree } from '@apollo/client/react/ssr';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import withApollo, { WithApolloProps } from '@sotnikov/next-with-apollo';
-import { register, unregister } from 'next-offline/runtime';
 import { DefaultSeo } from 'next-seo';
-import App from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import ReactGA from 'react-ga';
+import { useMount } from 'react-use';
 import AlertProvider from '../components/global/alert-provider';
 import SettingsProvider, { SettingsContext } from '../components/global/settings-provider';
 import introspectionResult from '../fragment-types';
@@ -48,9 +48,71 @@ const afterware = new ApolloLink((operation, forward) =>
   }),
 );
 
+const App: React.VFC<AppProps & WithApolloProps<NormalizedCache>> = ({
+  apollo,
+  Component,
+  pageProps,
+  router,
+}) => {
+  useMount(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    jssStyles?.parentNode?.removeChild(jssStyles);
+
+    ReactGA.initialize(process.env.GA_TRACKING_ID, {
+      debug: process.env.NODE_ENV === 'development',
+    });
+    ReactGA.plugin.require('ec');
+    ReactGA.pageview(router.asPath);
+
+    router.events.on('routeChangeComplete', (url: string) => {
+      ReactGA.pageview(url);
+    });
+  });
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+      </Head>
+      <ApolloProvider client={apollo}>
+        <SettingsProvider>
+          <SettingsContext.Consumer>
+            {(settings) => (
+              <DefaultSeo
+                title={settings.title ?? undefined}
+                description={settings.description ?? undefined}
+                canonical={absoluteURL(router.asPath)}
+                openGraph={{
+                  type: 'website',
+                  locale: 'en_US',
+                  url: absoluteURL(router.asPath),
+                  site_name: settings.title ?? undefined,
+                }}
+                twitter={{
+                  handle: '@artwithaliens',
+                  cardType: 'summary_large_image',
+                }}
+              />
+            )}
+          </SettingsContext.Consumer>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AlertProvider>
+              <Component {...pageProps} />
+            </AlertProvider>
+          </ThemeProvider>
+        </SettingsProvider>
+      </ApolloProvider>
+    </>
+  );
+};
+
 export default withApollo(
   ({ initialState }) =>
-    // Apollo GraphQL client.
     new ApolloClient({
       link: middleware.concat(
         afterware.concat(
@@ -64,81 +126,4 @@ export default withApollo(
         initialState || {},
       ),
     }),
-)(
-  class extends App<WithApolloProps<NormalizedCache>> {
-    componentDidMount() {
-      const { router } = this.props;
-
-      // Remove the server-side injected CSS.
-      const jssStyles = document.querySelector('#jss-server-side');
-      if (jssStyles?.parentNode != null) {
-        jssStyles.parentNode.removeChild(jssStyles);
-      }
-
-      if (process.env.NODE_ENV === 'production') {
-        register();
-      }
-
-      ReactGA.initialize(process.env.GA_TRACKING_ID, {
-        // debug: process.env.NODE_ENV === 'development',
-      });
-      ReactGA.plugin.require('ec');
-      ReactGA.pageview(router.asPath);
-
-      router.events.on('routeChangeComplete', (url: string) => {
-        ReactGA.pageview(url);
-      });
-    }
-
-    componentWillUnmount() {
-      if (process.env.NODE_ENV === 'production') {
-        unregister();
-      }
-    }
-
-    render() {
-      const { apollo, Component, pageProps, router } = this.props;
-
-      return (
-        <>
-          <Head>
-            <meta
-              name="viewport"
-              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-            />
-          </Head>
-          <ApolloProvider client={apollo}>
-            <SettingsProvider>
-              <SettingsContext.Consumer>
-                {(settings) => (
-                  <DefaultSeo
-                    title={settings.title ?? undefined}
-                    description={settings.description ?? undefined}
-                    canonical={absoluteURL(router.asPath)}
-                    openGraph={{
-                      type: 'website',
-                      locale: 'en_US',
-                      url: absoluteURL(router.asPath),
-                      site_name: settings.title ?? undefined,
-                    }}
-                    twitter={{
-                      handle: '@artwithaliens',
-                      cardType: 'summary_large_image',
-                    }}
-                  />
-                )}
-              </SettingsContext.Consumer>
-              <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <AlertProvider>
-                  <Component {...pageProps} />
-                </AlertProvider>
-              </ThemeProvider>
-            </SettingsProvider>
-          </ApolloProvider>
-        </>
-      );
-    }
-  },
-  { getDataFromTree },
-);
+)(App, { getDataFromTree });
