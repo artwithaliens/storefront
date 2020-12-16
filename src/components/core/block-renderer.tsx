88a92d64fe7173a0d4afb046247058f13190ace7
Eike Foken <eikefoken@gmail.com>
2020-12-16 17:21:35 +0100

    Fix TypeScript errors with new html-react-parser

--- src/components/core/block-renderer.tsx
+++ src/components/core/block-renderer.tsx
@@ -1,7 +1,7 @@
 import { BlockCover, BlockGallery, BlockImage, BlockParagraph } from '@components/blocks';
 import { RichText } from '@components/core';
 import clsx from 'clsx';
-import parse, { domToReact, HTMLReactParserOptions } from 'html-react-parser';
+import parse, { DOMNode, domToReact, HTMLReactParserOptions } from 'html-react-parser';
 import camelCase from 'lodash/camelCase';
 import mapKeys from 'lodash/mapKeys';
 import React from 'react';
@@ -15,27 +15,31 @@ type Props = {
 const BlockRenderer: React.FC<Props> = ({
   children,
   options = {
-    replace: ({ attribs = {}, children = [], name }) => {
-      const classNames = attribs.class?.split(/\s/) ?? [];
-      const style = mapKeys(styleToObject(attribs.style ?? ''), (_value, key) => camelCase(key));
+    replace: (node: DOMNode) => {
+      if ('attribs' in node) {
+        const { attribs = {}, children = [], name } = node;
+        const classNames = attribs.class?.split(/\s/) ?? [];
+        const style = mapKeys(styleToObject(attribs.style ?? ''), (_value, key) => camelCase(key));
 
-      return classNames.includes('wp-block-cover') ? (
-        <BlockCover className={clsx(classNames)} style={style}>
-          {domToReact(children, options)}
-        </BlockCover>
-      ) : classNames.includes('wp-block-gallery') ? (
-        <BlockGallery className={clsx(classNames)} style={style}>
-          {domToReact(children, options)}
-        </BlockGallery>
-      ) : classNames.includes('wp-block-image') ? (
-        <BlockImage className={clsx(classNames)} style={style}>
-          {domToReact(children, options)}
-        </BlockImage>
-      ) : name === 'p' ? (
-        <BlockParagraph className={clsx(classNames)} style={style}>
-          {domToReact(children, options)}
-        </BlockParagraph>
-      ) : null;
+        return classNames.includes('wp-block-cover') ? (
+          <BlockCover className={clsx(classNames)} style={style}>
+            {domToReact(children as DOMNode[], options)}
+          </BlockCover>
+        ) : classNames.includes('wp-block-gallery') ? (
+          <BlockGallery className={clsx(classNames)} style={style}>
+            {domToReact(children as DOMNode[], options)}
+          </BlockGallery>
+        ) : classNames.includes('wp-block-image') ? (
+          <BlockImage className={clsx(classNames)} style={style}>
+            {domToReact(children as DOMNode[], options)}
+          </BlockImage>
+        ) : name === 'p' ? (
+          <BlockParagraph className={clsx(classNames)} style={style}>
+            {domToReact(children as DOMNode[], options)}
+          </BlockParagraph>
+        ) : null;
+      }
+      return null;
     },
     trim: true,
   },
