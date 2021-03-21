b8042f6c39b60c946b951c716a4c3d6cd222a789
Eike Foken <eike.foken@aperto.com>
2021-03-21 19:15:29 +0100

    Fetch Insta feed via /api because of CORS headers

--- src/utils/fetchInstagramMedia.ts
+++ src/utils/fetchInstagramMedia.ts
@@ -10,58 +10,6 @@ export type InstagramMedia = {
   thumbnail?: string;
 };
 
-export default async function fetchInstagramMedia({
-  discardVideos = false,
-  numberOfMediaElements = 6,
-} = {}): Promise<InstagramMedia[]> {
-  const html = await (await fetch('https://www.instagram.com/artwithaliens/')).text();
-
-  const sharedData: {
-    entry_data?: {
-      ProfilePage?: {
-        graphql?: {
-          user?: {
-            edge_owner_to_timeline_media?: {
-              edges?: {
-                node: {
-                  __typename: string;
-                  accessibility_caption?: string;
-                  dimensions?: { height?: number; width?: number };
-                  display_url?: string;
-                  edge_liked_by?: { count?: number };
-                  edge_media_to_caption?: { edges?: { node?: { text?: string } }[] };
-                  edge_media_to_comment?: { count?: number };
-                  id: string;
-                  shortcode?: string;
-                  thumbnail_resources?: { src?: string }[];
-                };
-              }[];
-            };
-          };
-        };
-      }[];
-    };
-  } = JSON.parse(
-    html.match(/<script type="text\/javascript">window\._sharedData = (.*);<\/script>/)?.[1] ??
-      '{}',
-  );
-
-  return (
-    // Retrieve media info
-    sharedData.entry_data?.ProfilePage?.[0]?.graphql?.user?.edge_owner_to_timeline_media?.edges
-      ?.slice(0, numberOfMediaElements)
-      // Process only if is an image
-      ?.filter(({ node }) => !(discardVideos && node.__typename !== 'GraphImage'))
-      ?.map(({ node }) => ({
-        accessibilityCaption: node.accessibility_caption,
-        caption: node.edge_media_to_caption?.edges?.[0]?.node?.text,
-        commentsNumber: node.edge_media_to_comment?.count,
-        dimensions: node.dimensions,
-        displayImage: node.display_url,
-        id: node.id,
-        likes: node.edge_liked_by?.count,
-        postLink: `https://www.instagram.com/p/${node.shortcode}/`,
-        thumbnail: node.thumbnail_resources?.[2].src,
-      })) ?? []
-  );
+export default async function fetchInstagramMedia(): Promise<InstagramMedia[]> {
+  return (await fetch('/api/instagram')).json() as Promise<InstagramMedia[]>;
 }
