faeab22d1a11273f414998950c07bd2adecc327d
Eike Foken <eike.foken@aperto.com>
2021-04-13 19:21:22 +0200

    Improve Storybook config

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,40 +1,13 @@
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import { action } from '@storybook/addon-actions';
 import { DecoratorFunction } from '@storybook/addons';
 import { addDecorator, addParameters } from '@storybook/react';
-import { RouterContext } from 'next/dist/next-server/lib/router-context';
-import Router from 'next/router';
-import React, { useState } from 'react';
+import React from 'react';
+import { withNextRouter } from 'storybook-addon-next-router';
 import theme from '../src/components/ui/theme';
 
 addParameters({
   layout: 'centered',
 });
-
-const MockedRouter: React.FC = ({ children }) => {
-  const [pathname, setPathname] = useState('/');
-
-  const mockRouter: any = {
-    route: pathname,
-    pathname,
-    query: {},
-    asPath: pathname,
-    prefetch: async () => {},
-    push: async (newPathname: string) => {
-      action('Navigated')(newPathname);
-      setPathname(newPathname);
-      return true;
-    },
-  };
-
-  Router.router = mockRouter;
-
-  return <RouterContext.Provider value={mockRouter}>{children}</RouterContext.Provider>;
-};
-
-const withMockedRouter: DecoratorFunction<React.ReactElement> = (storyFn) => (
-  <MockedRouter>{storyFn()}</MockedRouter>
-);
 
 const withThemeProvider: DecoratorFunction<React.ReactElement> = (storyFn) => (
   <ThemeProvider theme={theme}>
@@ -43,5 +16,5 @@ const withThemeProvider: DecoratorFunction<React.ReactElement> = (storyFn) => (
   </ThemeProvider>
 );
 
-addDecorator(withMockedRouter);
+addDecorator(withNextRouter());
 addDecorator(withThemeProvider);
