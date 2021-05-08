d13ff11351087bd63a09f6b0a530725d0bae9482
Eike Foken <eike.foken@aperto.com>
2021-05-08 17:00:59 +0200

    Add more colors

--- src/colors.ts
+++ src/colors.ts
@@ -1,9 +1,12 @@
 const colors: { code: string; label: string; slug: string }[] = [
+  { code: '#3d3e3e', label: 'Anthracite', slug: 'anthracite' },
   { code: '#000', label: 'Black', slug: 'black' },
   { code: '#393732', label: 'Dark Heather Grey', slug: 'dark-heather-grey' },
   { code: '#0d745c', label: 'Earth Green', slug: 'earth-green' },
+  { code: '#b3b0ab', label: 'Heather Grey', slug: 'heather-grey' },
   { code: '#5e5532', label: 'Khaki', slug: 'khaki' },
   { code: '#df3d3d', label: 'Red', slug: 'red' },
+  { code: '#f5f5f5', label: 'White', slug: 'white' },
 ];
 
 export default colors;
