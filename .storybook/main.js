70212cb7c30d0d1ca18cb1baa25367734bf337a7
Eike Foken <eike.foken@aperto.com>
2021-05-09 23:37:47 +0200

    Fix TypeScript paths with Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,18 @@
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      plugins: config.resolve.plugins.concat([
+        new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+      ]),
+    },
+  }),
 };
