7a334d7f37df21efa270b996eacf2eb763838b11
Eike Foken <eike.foken@aperto.com>
2021-05-13 13:47:14 +0200

    Add pink color

--- src/colors.ts
+++ src/colors.ts
@@ -3,6 +3,7 @@ const colors: { code: string; label: string; slug: string }[] = [
   { code: '#000', label: 'Black', slug: 'black' },
   { code: '#393732', label: 'Dark Heather Grey', slug: 'dark-heather-grey' },
   { code: '#0d745c', label: 'Earth Green', slug: 'earth-green' },
+  { code: '#feaea3', label: 'Foxy Pink', slug: 'foxy-pink' },
   { code: '#b3b0ab', label: 'Heather Grey', slug: 'heather-grey' },
   { code: '#4a4940', label: 'Khaki', slug: 'khaki' },
   { code: '#5e5532', label: 'Mushroom', slug: 'mushroom' },
